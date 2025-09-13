c7911a363985a24f58b16e17460c3edd2c1cd750
LeVuWan <levuwan@gmail.com>
2025-09-13 10:12:06 +0700

    setup connect db

---
+++ src/main/java/com/windy/HelloController.java
@@ -0,0 +1,17 @@
+package com.windy;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloController {
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page";
+    }
+    
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page";
+    }
+}
